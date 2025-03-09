6560e764bebcf2bb8fc9094b0e5db3ffab138809
waterchengnai <1794685881@qq.com>
2025-03-09 16:23:42 +0800

    Add files via upload

---
+++ 03用&起别名.cpp
@@ -0,0 +1,31 @@
+//引用基本语法
+
+//数据类型 &别名 = 原名
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	int a = 10;
+//	int &b = a;
+//	cout << "a= " << a << endl; //10
+//	cout << "b= " << b << endl; //10
+//
+//	b = 100;
+//	cout << "a= " << a << endl; //100
+//	cout << "b= " << b << endl; //100
+//
+//	int c = 20;
+//	b = c;//这里并非更改别名的对象，而是对b赋值
+//	cout << "a= " << a << endl; //20
+//	cout << "b= " << b << endl; //20
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//注意事项
+//1.引用不许初始化
+//		int &b=; 这样不初始化的定义是错误的
+//2.引用一旦初始化，就不可以更改为别人的别名
+
